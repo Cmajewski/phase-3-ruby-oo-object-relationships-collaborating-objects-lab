d5370d04635df8bd6432cd53a041e249bf40a36a
github username <github email address>
2022-07-02 11:44:30 -0400

    commit tree 6328d93298a450fcb0cc7df9ff8529d37fc32e23

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,19 @@
+class MP3Importer
+attr_accessor :path
+
+    def initialize(path)
+        @path=path
+    end 
+
+    def files
+        @files=Dir.glob("#{path}/*.mp3").collect do |x|
+          x.gsub("#{path}/","")
+        end
+      end
+    
+      def import
+        files.each {|file| Song.new_by_filename(file)}
+    
+      end
+
+end 
